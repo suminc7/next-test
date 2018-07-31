383051508b76ef19f7a2bfd471a89c246a8a8a2a
bran <suminc7@naver.com>
2018-07-31 15:20:03 +0900

    test sass

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,21 @@
 import Layout from '../components/MyLayout.js'
 import Link from 'next/link'
 import fetch from 'isomorphic-unfetch'
+import '../styles/style.scss'
 
 const Index = (props) => (
     <Layout>
         <h1>Batman TV Shows</h1>
+        <style jsx>{`
+          li {
+            font-size: 14px;
+          }
+        `}</style>
+            <style global jsx>{`
+          body {
+            background: white;
+          }
+        `}</style>
         <ul>
             {props.shows.map(({show}) => (
                 <li key={show.id}>
@@ -14,6 +25,8 @@ const Index = (props) => (
                 </li>
             ))}
         </ul>
+        <button>button</button>
+        <img width={ 100 } src="/static/images/img-about-shirts-01.png" alt="my image" />
     </Layout>
 )
 
